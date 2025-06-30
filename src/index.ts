7a03be10484e25e725e6e6d985df3929c2ac49ca
noah227 <noah227@foxmail.com>
2025-06-30 10:05:55 +0800

    chore: update package description

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-export const ljust = (s: string, w: number, fillChars = " ") => {
+export const ljust = (s: string, w: number, fillChar = " ") => {
     const rPadding = w > s.length ? w - s.length : 0
-    return s + fillChars.repeat(rPadding)
+    return s + fillChar.repeat(rPadding)
 }
 
-export const rjust = (s: string, w: number, fillChars = " ") => {
+export const rjust = (s: string, w: number, fillChar = " ") => {
     const lPadding = w > s.length ? w - s.length : 0
-    return fillChars.repeat(lPadding) + s
+    return fillChar.repeat(lPadding) + s
 }
 
-export const center = (s: string, w: number, fillChars = " ") => {
+export const center = (s: string, w: number, fillChar = " ") => {
     if (w <= s.length) return s
     const lp = Math.floor((w - s.length) / 2)
     const rp = w - s.length - lp
-    return fillChars.repeat(lp) + s + fillChars.repeat(rp)
+    return fillChar.repeat(lp) + s + fillChar.repeat(rp)
 }
 
 export const justify = {ljust, rjust, center}
