fd8de32280c1c871a5ba1ab04f85e54cf0e784ff
noah227 <noah227@foxmail.com>
2025-06-30 10:20:24 +0800

    refactor!: unified funtion realization & add fillChar length limit(1)

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,35 @@
+const _justify = (s: string, w: number, style: "left" | "right" | "center", fillChar = " ") => {
+    if (fillChar.length > 1) return console.error("The fill character must be exactly one character long")
+    let lPadding: number, rPadding: number
+    switch (style) {
+        case "left":
+            rPadding = w > s.length ? w - s.length : 0
+            break
+        case "right":
+            lPadding = w > s.length ? w - s.length : 0
+            break
+        case "center":
+            if (w > s.length) {
+                lPadding = Math.floor((w - s.length) / 2)
+                rPadding = w - s.length - lPadding
+            }
+            break
+    }
+    lPadding ??= 0
+    rPadding ??= 0
+    return [fillChar.repeat(lPadding), s, fillChar.repeat(rPadding)].join("")
+}
+
 export const ljust = (s: string, w: number, fillChar = " ") => {
-    const rPadding = w > s.length ? w - s.length : 0
-    return s + fillChar.repeat(rPadding)
+    return _justify(s, w, "left", fillChar)
 }
 
 export const rjust = (s: string, w: number, fillChar = " ") => {
-    const lPadding = w > s.length ? w - s.length : 0
-    return fillChar.repeat(lPadding) + s
+    return _justify(s, w, "right", fillChar)
 }
 
 export const center = (s: string, w: number, fillChar = " ") => {
-    if (w <= s.length) return s
-    const lp = Math.floor((w - s.length) / 2)
-    const rp = w - s.length - lp
-    return fillChar.repeat(lp) + s + fillChar.repeat(rp)
+    return _justify(s, w, "center", fillChar)
 }
 
 export const justify = {ljust, rjust, center}
