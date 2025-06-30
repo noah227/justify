271d68119ac88b0ab22fe4e47432a01fa8cf8f8f
noah227 <noah227@foxmail.com>
2025-06-30 10:02:31 +0800

    chore: use space as default fill character

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-export const ljust = (s: string, w: number, fillChars = "") => {
+export const ljust = (s: string, w: number, fillChars = " ") => {
     const rPadding = w > s.length ? w - s.length : 0
     return s + fillChars.repeat(rPadding)
 }
 
-export const rjust = (s: string, w: number, fillChars = "") => {
+export const rjust = (s: string, w: number, fillChars = " ") => {
     const lPadding = w > s.length ? w - s.length : 0
     return fillChars.repeat(lPadding) + s
 }
 
-export const center = (s: string, w: number, fillChars = "") => {
+export const center = (s: string, w: number, fillChars = " ") => {
     if (w <= s.length) return s
     const lp = Math.floor((w - s.length) / 2)
     const rp = w - s.length - lp
